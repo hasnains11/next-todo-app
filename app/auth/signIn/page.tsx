8905af79191731da1a3ef36c39e0abb75c1f3fcb
hasnains11 <hasnain244saeed@gmail.com>
2025-01-22 21:27:54 +0500

    initial conmit

---
+++ app/auth/signIn/page.tsx
@@ -0,0 +1,13 @@
+import { useSession } from 'next-auth/react'
+import React from 'react'
+
+const page = () => {
+
+    const session = useSession();
+    console.log(session, "Session");
+    return (
+        <div>Sign In Page</div>
+    )
+}
+
+export default page
